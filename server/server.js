88caebcb63de4647d577866190b6c958cc98f838
Felix Pohl <felix.pohl@brandad-systems.de>
2022-06-18 15:49:25 +0200

    fix cors errors

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ let latestId = 1;
 const products = [];
 
 app.use(express.json()) // for parsing application/json
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
